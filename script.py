7c84e8951bf8be9db326e594f47caf7688486f88
Carolina Sanchez <jcsanchezl3@outlook.es>
2020-12-21 17:24:18 -0500

    Agregado datos del proyecto

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 # Script de prueba para Git
-# Autor: Carolina Sanchez <jcsanchezl3@outlook.es>
+# Autor: Carolina Sánchez <jcsanchezl3@outlook.es>
 # Fecha: 21-12-2020
 # Versión: 1.0.0
 def main():
-print("Hola Mundo!")
+nombre = input("Cuál es tu nombre? ")
+print("Hola",nombre,"Bienvenido a Git!")
 if __name__ == "__main__":
 main()
